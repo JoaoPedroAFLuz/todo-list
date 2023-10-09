1734098c46f9f1c0f1e3bab4e18f105ee817b351
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-10-09 12:04:56 -0300

    criar projeto

---
+++ src/test/java/br/com/joaopedroafluz/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaopedroafluz.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
