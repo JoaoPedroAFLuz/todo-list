e9441843421188ea1688c9a7fe06add515a6b0dd
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-10-10 23:58:54 -0300

    cadastrar usuario

--- src/main/java/br/com/joaopedroafluz/todolist/user/UserController.java
+++ src/main/java/br/com/joaopedroafluz/todolist/user/UserController.java
@@ -1,17 +1,28 @@
 package br.com.joaopedroafluz.todolist.user;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping()
-    public void create(@RequestBody UserModel user) {
-        System.out.println(user.getName());
+    public ResponseEntity create(@RequestBody UserModel user) {
+        var registeredUser = this.userRepository.findByUsername(user.getUsername());
+
+        if (registeredUser != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username já está em uso");
+        }
+
+        var userRegistered = this.userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRegistered);
     }
 
 }
