fbc31c6eaae5e2e0ee64c64c22928e236f5b9511
João Pedro Luz <70025328+JoaoPedroAFLuz@users.noreply.github.com>
2023-10-11 22:49:28 -0300

    criptografar senha do usuario ao cadastrar

--- src/main/java/br/com/joaopedroafluz/todolist/user/UserController.java
+++ src/main/java/br/com/joaopedroafluz/todolist/user/UserController.java
@@ -1,9 +1,13 @@
 package br.com.joaopedroafluz.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
@@ -19,6 +23,10 @@ public class UserController {
         if (registeredUser != null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username já está em uso");
         }
+
+        String hashedPassword = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
+
+        user.setPassword(hashedPassword);
 
         var userRegistered = this.userRepository.save(user);
 
